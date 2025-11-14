5c59a867df0ac57bfb07cab97e6035d32c7a65e0
Ratan Prajapati <ratanprajapati1242@gmail.com>
2025-11-14 12:47:18 +0530

    content size upgraded

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -20,14 +20,14 @@ const HeroSection = () => {
             <div className="absolute inset-0 bg-neutral-900/40 backdrop-blur-[1px]"></div>
             <div className="relative z-10 text-center max-w-4xl px-4">
 
-                <p className="text-[24px]   font-medium text-white/90 mb-3 tracking-widest uppercase">
+                <p className="text-[16px]   font-medium text-white/90 mb-3 tracking-widest uppercase">
                     Buy and Sell Industrial Machinery Online with Ease.
                 </p>
-                <h1 className="text-[40px]   text-white leading-tight mb-6 drop-shadow-lg">
+                <h1 className="text-[30px]   text-white leading-tight mb-6 drop-shadow-lg">
 
                     Transforming Global Trade in Foundry Metals, Smart Manufacturing, and Industrial Engineering
                 </h1>
-                <p className="text-[16px]   text-white/90 leading-relaxed font-light mx-auto max-w-3xl">
+                <p className="text-[11px]   text-white/90 leading-relaxed font-light mx-auto max-w-3xl">
                     Mega Foundries delivers a comprehensive portfolio of products, including turbines, oil & gas equipment, water management technologies, and power generation systems. We also enable seamless access to FPC services, empowering businesses to execute large-scale industrial projects with confidence.
                 </p>
             </div>
