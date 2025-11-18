b19f50c0abb8b259c68d5dbc6143ae165d9ef20a
Ratan Prajapati <ratanprajapati1242@gmail.com>
2025-11-18 19:12:53 +0530

    footer updated

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -7,14 +7,63 @@ import React from 'react'
 
 
 const Footer = () => {
-
-
+  const products = [
+    { lable: 'Molded Gratings', href: "/" },
+    { lable: 'Pultruded Grating', href: "/" },
+    { lable: 'Dynaform Structural Shapes', href: "/" },
+    { lable: 'Dynarail/DynaRound Guardrail & Handrail Products', href: "/" },
+    { lable: 'Dynarail Ladder Products', href: "/" },
+    { lable: 'Stair Solutions', href: "/" },
+    { lable: 'Fiberglass Plate', href: "/" },
+    { lable: 'Work Platforms', href: "/" },
+    { lable: 'Accessories / Complementary Products', href: "/" },
+    { lable: 'Custom Pultrusions', href: "/" },
+    { lable: 'Metering Pump Stands', href: "/" },
+    { lable: 'Rooftop Safety Products', href: "/" },
+    { lable: 'Unique Product Solutions', href: "/" }
+  ]
+  const market = [
+    { lable: 'Market Solutions', href: "/" },
+    { lable: 'Architectural & Commercial', href: "/" },
+    { lable: 'Chemical', href: "/" },
+    { lable: 'Data Center Solutions', href: "/" },
+    { lable: 'Docks & Decking', href: "/" },
+    { lable: 'Food & Beverage', href: "/" },
+    { lable: 'General Manufacturing', href: "/" },
+    { lable: 'Metals & Mining', href: "/" },
+    { lable: 'Oil & Gas', href: "/" },
+    { lable: 'Pharmaceutical', href: "/" },
+    { lable: 'Pulp & Paper', href: "/" },
+    { lable: 'Recreational', href: "/" },
+    { lable: 'Technology', href: "/" },
+    { lable: 'Telecommunications', href: "/" },
+    { lable: 'Transportation', href: "/" },
+    { lable: 'Utilities & Power', href: "/" },
+    { lable: 'Water & Wastewater', href: "/" },
+  ]
+  const resourceCenter = [
+    { lable: 'Product Brochures', href: "/" },
+    { lable: 'Market Overviews', href: "/" },
+    { lable: 'Case Studies', href: "/" },
+    { lable: 'Specifications', href: "/" },
+    { lable: 'Design Guides', href: "/" },
+    { lable: 'CAD Drawings/Details', href: "/" },
+    { lable: 'Installation Guides', href: "/" },
+    { lable: 'Other Technical Documents', href: "/" },
+    { lable: 'Certifications', href: "/" },
+    { lable: 'Videos', href: "/" },
+    { lable: 'SDS (formerly MSDS)', href: "/" },
+    { lable: 'Infographics', href: "/" },
+    { lable: 'FAQ', href: "/" },
+    { lable: 'Glossary', href: "/" },
+    { lable: 'Design Resources', href: "/" }
+  ]
   return (
     <footer className="bg-neutral-800 text-white relative">
       <div className="container mx-auto px-4 sm:px-6 lg:px-14 pt-12 pb-8">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-10">
 
-          <div>
+          <div className='flex flex-col'>
             <div className="mb-4">
               <Link href={'/'}><Image src={'/logo.jpeg'} alt='Logo' height={100} width={100} /></Link>
 
@@ -36,32 +85,42 @@ const Footer = () => {
               <Link href={'/'} className='p-2 border rounded-3xl'><Twitter className='w-4 h-4' /></Link>
             </div>
           </div>
-
           <div className='flex flex-col'>
             <h4 className="text-xl font-semibold mb-5 text-white relative pb-3">
-              Quick Link
+              Products
               {/* Red underline accent */}
               <span className="absolute left-0 bottom-0 h-0.5 w-1/4 bg-[#cc2221]"></span>
             </h4>
-            <Link href={'/'} className="text-neutral-400zz pb-3 hover:text-red-500 transition-colors text-sm font-light"> About Us </Link>
-            <Link href={'/'} className="pb-3 hover:text-[#cc2221] transition-colors text-[11px]"> Our Products </Link>
-            <Link href={'/'} className="pb-3 hover:text-[#cc2221] transition-colors text-[11px]"> Contact Us </Link>
-            <Link href={'/'} className="pb-3 hover:text-[#cc2221] transition-colors text-[11px]"> Blog </Link>
-            <Link href={'/'} className="pb-3 hover:text-[#cc2221] transition-colors text-[11px]"> FAQ's </Link>
-            <Link href={'/'} className="pb-3 hover:text-[#cc2221] transition-colors text-[11px]"> Privacy Policy </Link>
-            <Link href={'/'} className="pb-3 hover:text-[#cc2221] transition-colors text-[11px]"> User data deletion instructions </Link>
+            {products.map((item, index) => (
+              <Link key={index} href={item.href} className="pb-3 hover:text-[#cc2221] transition-colors text-[11px]"> {item.lable}</Link>
+            ))}
+
           </div>
           <div className='flex flex-col'>  <h4 className="text-xl font-semibold mb-5 text-white relative pb-3">
-            Main Products
+            Markets
             {/* Red underline accent */}
             <span className="absolute left-0 bottom-0 h-0.5 w-1/4 bg-[#cc2221]"></span>
           </h4>
-            <Link href={'/'} className="pb-3 hover:text-[#cc2221] transition-colors text-[11px]"> Pipeline and Water System </Link>
-            <Link href={'/'} className="pb-3 hover:text-[#cc2221] transition-colors text-[11px]"> Turbine </Link>
-            <Link href={'/'} className="pb-3 hover:text-[#cc2221] transition-colors text-[11px]"> Pumps </Link>
-            <Link href={'/'} className="pb-3 hover:text-[#cc2221] transition-colors text-[11px]"> Transformer </Link>
+            {market.map((item, index) => (
+              <Link key={index} href={'/'} className="pb-3 hover:text-[#cc2221] transition-colors text-[11px]"> {item.lable} </Link>
+            ))}
           </div>
-
+          <div className='flex flex-col'>  <h4 className="text-xl font-semibold mb-5 text-white relative pb-3">
+            Resource Center
+            {/* Red underline accent */}
+            <span className="absolute left-0 bottom-0 h-0.5 w-1/4 bg-[#cc2221]"></span>
+          </h4>
+            {resourceCenter.map((item, index) => (
+              <Link key={index} href={'/'} className="pb-3 hover:text-[#cc2221] transition-colors text-[11px]"> {item.lable} </Link>
+            ))}
+            <Link href={'/'} className="pb-3 hover:text-[#cc2221] transition-colors text-sm"> About Us </Link>
+            <Link href={'/'} className="pb-3 hover:text-[#cc2221] transition-colors text-sm"> Design Resources </Link>
+            <Link href={'/'} className="pb-3 hover:text-[#cc2221] transition-colors text-sm"> Contact Us </Link>
+            <Link href={'/'} className="pb-3 hover:text-[#cc2221] transition-colors text-sm"> Where to Buy </Link>
+            <Link href={'/'} className="pb-3 hover:text-[#cc2221] transition-colors text-sm"> California Supply Chain Act </Link>
+            <Link href={'/'} className="pb-3 hover:text-[#cc2221] transition-colors text-sm"> European Accessibility Notice </Link>
+            <Link href={'/'} className="pb-3 hover:text-[#cc2221] transition-colors text-sm"> Sitemap </Link>
+          </div>
           <div>
             <h4 className="text-xl font-semibold mb-5 text-white relative pb-3">
               Subscribe
