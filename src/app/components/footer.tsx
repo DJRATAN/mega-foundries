60d5691813604f76e9828c580c92e32b5d14213b
Ratan Prajapati <ratanprajapati1242@gmail.com>
2025-11-29 07:58:52 +0530

    footer also udpated

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -154,7 +154,7 @@ const Footer = () => {
   return (
     // <footer className="bg-neutral-200 relative">
     <footer className="bg-neutral-800 text-white relative">
-      <div className="container mx-auto px-4 sm:px-6 lg:px-14 pt-12 pb-8">
+      <div className="w-full px-4 sm:px-6 lg:px-10 pt-12 pb-8">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-10">
 
           <div className='flex flex-col'>
@@ -268,7 +268,7 @@ const Footer = () => {
 
         </div>
       </div>
-      <div className="container border-t mx-auto px-4 sm:px-6 lg:px-14 pt-12 pb-8">
+      <div className="w-full px-4 sm:px-6 lg:px-10 pt-12 pb-8">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-10">
           <div className='flex flex-col'>  <h4 className="text-xl font-semibold mb-5 relative pb-3">
             CORPORATE
